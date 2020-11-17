52b5516bc450c5f8a0047657f7d586ff85043e28
Nur Fajar Ismail <nur.fajar.ft18@mail.umy.ac.id>
2020-11-17 15:12:21 +0700

    Add project files.

---
+++ IService1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        double add(double num1, double num2);
+        [OperationContract]
+        double sub(double num1, double num2);
+        [OperationContract]
+        double multiple(double num1, double num2);
+        [OperationContract]
+        double divide(double num1, double num2);
+    }
+
+
+    
+}
